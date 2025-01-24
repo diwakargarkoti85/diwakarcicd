ff7b6f0dcdbb01ff85c503ba7f19e7363498fd03
diwakargarkoti85 <diwakargarkoti@gmail.com>
2025-01-24 14:20:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
+# Data block to fetch the Key Vault information
+data "azurerm_key_vault" "azure_vault" {
+  name                = var.keyvault_name
+  resource_group_name = var.keyvault_rg
+}
+
+# Data block to get the SSH public key from the Key Vault
+data "azurerm_key_vault_secret" "ssh_public_key" {
+  name         = var.sshkvsecret
+  key_vault_id = data.azurerm_key_vault.azure_vault.id
+}
+
+# Data block to get the Service Principal ID from the Key Vault
+data "azurerm_key_vault_secret" "spn_id" {
+  name         = var.clientidkvsecret
+  key_vault_id = data.azurerm_key_vault.azure_vault.id
+}
+
+# Data block to get the Service Principal Secret from the Key Vault
+data "azurerm_key_vault_secret" "spn_secret" {
+  name         = var.spnkvsecret
+  key_vault_id = data.azurerm_key_vault.azure_vault.id
+}
+
+# Resource Group definition
 resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group
   location = var.azure_region
 }
 
+# Virtual Network definition for AKS
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.aks_vnet_name
   resource_group_name = azurerm_resource_group.aks_rg.name
@@ -10,6 +36,7 @@ resource "azurerm_virtual_network" "aks_vnet" {
   address_space       = var.vnetcidr
 }
 
+# Subnet definition for AKS
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks_subnet"
   resource_group_name  = azurerm_resource_group.aks_rg.name
@@ -17,12 +44,14 @@ resource "azurerm_subnet" "aks_subnet" {
   address_prefixes     = var.subnetcidr
 }
 
+# AKS Cluster definition
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = var.dns_name
 
+  # Default node pool for AKS
   default_node_pool {
     name            = var.agent_pools.name
     node_count      = var.agent_pools.count
@@ -30,6 +59,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     os_disk_size_gb = var.agent_pools.os_disk_size_gb
   }
 
+  # Linux profile with SSH key for the admin user
   linux_profile {
     admin_username = var.admin_username
     ssh_key {
@@ -37,15 +67,18 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     }
   }
 
+  # Role-based access control enabled
   role_based_access_control {
     enabled = true
   }
 
+  # Service Principal for the AKS Cluster
   service_principal {
     client_id     = data.azurerm_key_vault_secret.spn_id.value
     client_secret = data.azurerm_key_vault_secret.spn_secret.value
   }
 
+  # Tags for the AKS Cluster
   tags = {
     Environment = "Demo"
   }
